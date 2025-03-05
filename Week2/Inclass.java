7f6cfdddca80085b54d4cd149fb214383987c799
VincentWang2961 <vincentwang2961@gmail.com>
2025-03-05 10:53:03 +0800

    Inclass

---
+++ Week2/Inclass.java
@@ -0,0 +1,22 @@
+public class Inclass {
+    public static void main(String[] args) {
+        int a = 2, b = 3, c = 4;
+        //c = 4;
+        if (a + b > c) {
+            c = a + b;
+            a = b;
+        }
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+
+        // Gives value of i to x, then increments i by 1
+        int i = 1;
+        int x = i++;
+        System.out.println("i = " + i + " x = " + x);
+
+        // Increments x by 1, then gives the value to y
+        int y = x += 1;
+        System.out.println("i = " + i + " x = " + x + " y = " + y);
+    }
+}
