772e1b36ab2432e32e6616ddd18b17893c772df6
VincentWang2961 <vincentwang2961@gmail.com>
2025-03-19 08:50:36 +0800

    Basic guess number game

---
+++ Labs/lab3/exercise1.java
@@ -0,0 +1,22 @@
+package Labs.lab3;
+
+import java.util.Scanner;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        int secretNumber = 9;
+        System.out.println("I am thinking of a number between 1 and 10. Guess what it is.");
+        
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        
+        do {
+            guess = scanner.nextInt();
+            if (guess != secretNumber) {
+                System.out.println("Nope, try again.");
+            }
+        } while (guess != secretNumber);
+        
+        System.out.println("You got it!");
+    }
+}
