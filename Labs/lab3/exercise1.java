b8b9f57e1e98f5c95b923bed1d6b447818f7a0bf
VincentWang2961 <vincentwang2961@gmail.com>
2025-03-19 08:54:16 +0800

    Random guess number game

--- Labs/lab3/exercise1.java
+++ Labs/lab3/exercise1.java
@@ -1,19 +1,26 @@
 package Labs.lab3;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class exercise1 {
     public static void main(String[] args) {
-        int secretNumber = 9;
+
+        // Pick a random number between 1 and 100
+        int secretNumber = new Random().nextInt(100) + 1;
+
         System.out.println("I am thinking of a number between 1 and 10. Guess what it is.");
         
         Scanner scanner = new Scanner(System.in);
         int guess;
         
+        // Guessing loop
         do {
             guess = scanner.nextInt();
-            if (guess != secretNumber) {
-                System.out.println("Nope, try again.");
+            if (guess > secretNumber) {
+                System.out.println("Too high, try again.");
+            } else if (guess < secretNumber) {
+                System.out.println("Too low, try again.");
             }
         } while (guess != secretNumber);
         
